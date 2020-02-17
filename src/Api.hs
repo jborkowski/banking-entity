29cea533fe77fddd3d5b77aca23bc9fec0315fec
Jonatan Borkowski <jonatan.borkowski@pm.me>
2020-02-17 01:32:06 +0100

    Compose two APIs

--- src/Api.hs
+++ src/Api.hs
@@ -18,13 +18,19 @@ import Servant
   )
 import Servant.Server
 
+accountApp :: Config -> Server AccountAPI
+accountApp cfg = hoistServer accountApi (convertApp cfg) accountServer
+
+operationsApp :: Config -> Server OperationsAPI
+operationsApp cfg = hoistServer operationsApi (convertApp cfg) operationsServer
+
 appToServer :: Config -> Server AppAPI
-appToServer cfg = hoistServer accountApi (convertApp cfg) accountServer
+appToServer cfg = accountApp cfg :<|> operationsApp cfg
 
 convertApp :: Config -> AppT IO a -> Handler a
 convertApp cfg appt = Handler $ runReaderT (runApp appt) cfg
 
-type AppAPI = AccountAPI -- :<|> OperationsAPI
+type AppAPI = AccountAPI :<|> OperationsAPI
 
 appApi :: Proxy AppAPI
 appApi = Proxy
